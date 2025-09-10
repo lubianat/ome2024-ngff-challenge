555bdb74ca376ded856b48f1fc249b4bd28301f4
Tiago Lubiana <tiagolubiana@gmail.com>
2025-09-10 13:09:16 -0300

    fix: avoid CORS issues

--- ome2024-ngff-challenge/vite.config.js
+++ ome2024-ngff-challenge/vite.config.js
@@ -37,8 +37,8 @@ let config = {
 };
 // this will be undefined when deployed from netlify, but is used by gh-pages
 if (process.env.GITHUB_REPOSITORY_OWNER) {
-  config.base = `https://${process.env.GITHUB_REPOSITORY_OWNER}.github.io/ome2024-ngff-challenge/`;
+  // Deploying on GitHub Pages (repo is in a subpath)
+  config.base = '/ome2024-ngff-challenge/';
 }
-
 // https://vitejs.dev/config/
 export default defineConfig(config);
